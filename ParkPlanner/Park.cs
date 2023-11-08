7f0255505692f8b04b88b987f6f3627e1618699b
is7012bechmana <105136809+bechmana@users.noreply.github.com>
2023-11-08 14:50:00 -0500

    Try again
    Trying to send again

--- ParkPlanner/Park.cs
+++ ParkPlanner/Park.cs
@@ -12,8 +12,6 @@ namespace ParkPlannerPark
     using System.Collections.Generic;
 
     using System.Globalization;
-    using System.Text.Json.Serialization;
-    using System.Text.Json;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
 
